2bed412463f69e652a49696d2b896466f3d55766
cwd <sadasds>
2015-10-24 23:40:44 +0000

    1.初始化项目

---
+++ ios5/ios5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios5
+//
+//  Created by Wanna on 15/10/24.
+//  Copyright © 2015年 Wanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
